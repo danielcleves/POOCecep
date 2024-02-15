bdb32bd18b736628bc3ceaa48e63c8e221ad51be
DeatGame <daniel.cleves.rueda@gmail.com>
2024-02-14 22:44:36 -0500

    Cambio de inicialización de la variable dFecNac Para prueba de github

--- src/Variables/Varx.java
+++ src/Variables/Varx.java
@@ -4,30 +4,27 @@
  * and open the template in the editor.
  */
 package Variables;
+
 //estoy debajo del paquete
-
-
 
 /**
  *
  * @author estudiantes
  */
 public class Varx {
-private static int idCli=4510;
-public static String sNompac="Elvira";
-public static String sApapac="Millan";
-private static char cGenpac='F';
-private static String dFecNac="01/01/2000";
-public static final double dPesopac=135.5;
-private static float fEstapac= 1.75f;
 
+  private static int idCli = 4510;
+  public static String sNompac = "Elvira";
+  public static String sApapac = "Millan";
+  private static char cGenpac = 'F';
+  private static String dFecNac = "01/01/2001";
+  public static final double dPesopac = 135.5;
+  private static float fEstapac = 1.75f;
 
-private static String sPalabra="Hospital Pacientes Cronicos";
-    public static void main(String[] args) {
-        System.out.println("\t\t\t"+sPalabra+"\t\t\t");
-        System.out.print("\nIdentificacion: " + idCli + 
-                "\nNombre: "+ sNompac);
-        
-    }
-    
+  private static String sPalabra = "Hospital Pacientes Cronicos";
+
+  public static void main(String[] args) {
+    System.out.println("\t\t\t" + sPalabra + "\t\t\t");
+    System.out.print("\nIdentificacion: " + idCli + "\nNombre: " + sNompac);
+  }
 }
