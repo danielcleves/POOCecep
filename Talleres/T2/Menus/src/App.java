f016d3333361273ae1f554f0721b1b438193c145
DeatGame <daniel.cleves.rueda@gmail.com>
2024-03-02 01:15:53 -0500

    Se añaden funcionalidades de caracteres y se encierra en bucles para que la q devuelva al menú anterior

--- Talleres/T2/Menus/src/App.java
+++ Talleres/T2/Menus/src/App.java
@@ -12,49 +12,96 @@ import java.io.InputStreamReader;
 public class App {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String[][][] menus = { { { "Caracteres" }, { "Mayúsculas", "Minúsculas" } }, { { "Numéricos" } },
-                { { "Comodines" } } };
+        String[][][] menus = {
+                { { "Caracteres" }, { "Mayúsculas", "Minúsculas" } },
+                { { "Numéricos" }, {} },
+                { { "Comodines" }, {} }
+        };
         int iSelec, iSubSelec; // opción, subopción seleccionada
-        String sSelec; // Guarda la opción selecionada, si es una sub opción se guarda como
-                       // "opción > subopción"
+        String sSelec, rSelec, rSubSelec;
+        boolean exit = false, haveSec = false;
 
-        int i = 1;
-        for (String[][] menu : menus) {
-            System.out.println(i + ") " + menu[0][0]);
-            i++;
-        }
-        System.out.print("Q) Salir\nEscribe la opción: ");
-        iSelec = Integer.parseInt(reader.readLine()) - 1;
-        sSelec = menus[iSelec][0][0];
-
-        switch (sSelec.toLowerCase()) {
-            case "numéricos":
-                System.out.println("\n┌-------------------┐");
-                System.out.println("| Valores numericos |");
-                System.out.println("├-------------------┤");
-                for (int j = 0; j < 10; j++) {
-                    System.out.println("|         " + j + "         |");
-                }
-                System.out.println("└-------------------┘");
-                break;
-        }
-
-        if (menus[iSelec][1].length >= 1) {
-            i = 1;
-            for (String option : menus[iSelec][1]) {
-                System.out.println(i + ") " + option);
+        while (!exit) {
+            int i = 1;
+            System.out.println("Menu principal:");
+            for (String[][] menu : menus) {
+                System.out.println("\t" + i + ") " + menu[0][0]);
                 i++;
             }
-            System.out.print("Q) Salir\nEscribe la opción: ");
-            iSubSelec = Integer.parseInt(reader.readLine()) - 1;
-            sSelec += " > " + menus[iSelec][1][iSubSelec];
+            System.out.print("\tQ) Salir\nEscribe la opción: ");
+            rSelec = reader.readLine();
+            try {
+                iSelec = Integer.parseInt(rSelec) - 1;
+            } catch (Exception e) {
+                exit = true;
+                break;
+            }
+            sSelec = menus[iSelec][0][0];
 
-            // Aquí se validará si la respuesta es una respuesta final y si lo es se
-            // ejecutará lo pedido
-        } else {
+            switch (sSelec.toLowerCase()) {
+                case "numéricos":
+                    System.out.println("\n┌-------------------┐");
+                    System.out.println("| Valores numéricos |");
+                    System.out.println("├-------------------┤");
+                    for (int j = 0; j < 10; j++) {
+                        System.out.println("|         " + j + "         |");
+                    }
+                    System.out.println("└-------------------┘");
+                    break;
+                default:
+                    haveSec = true;
+                    break;
+            }
 
+            if (haveSec) {
+                haveSec = false;
+                if ((menus[iSelec][1].length >= 1)) {
+                    while (!exit) {
+                        System.out.println("\nMenú " + sSelec);
+                        i = 1;
+                        for (String option : menus[iSelec][1]) {
+                            System.out.println("\t" + i + ") " + option);
+                            i++;
+                        }
+                        System.out.print("\tQ) Salir\nEscribe la opción: ");
+                        rSubSelec = reader.readLine();
+                        try {
+                            iSubSelec = Integer.parseInt(rSubSelec) - 1;
+                        } catch (Exception e) {
+                            exit = true;
+                            break;
+                        }
+                        System.out.println(sSelec.toLowerCase());
+
+                        switch ((sSelec + " > " + menus[iSelec][1][iSubSelec]).toLowerCase()) {
+                            case "caracteres > mayúsculas":
+                                System.out.println("\n┌-----------------------┐");
+                                System.out.println("| Caracteres mayúsculas |");
+                                System.out.println("├-----------------------┤");
+                                for (int j = 65; j <= 90; j++) {
+                                    System.out.println("|           " + (char) j + "           |");
+                                }
+                                System.out.println("└-----------------------┘");
+                                break;
+                            case "caracteres > minúsculas":
+                                System.out.println("\n┌---------------------┐");
+                                System.out.println("| Caracteres minúsculas |");
+                                System.out.println("├---------------------┤");
+                                for (int j = 97; j <= 122; j++) {
+                                    System.out.println("|          " + (char) j + "          |");
+                                }
+                                System.out.println("└---------------------┘");
+                                break;
+                        }
+                    }
+                    exit = false;
+                } else {
+                    System.out.println("\n┌---------------------------------------------------------┐");
+                    System.out.println("| Los submenús que buscas todavía no han sido codificados |");
+                    System.out.println("└---------------------------------------------------------┘");
+                }
+            }
         }
-
-        System.out.println(sSelec);
+        System.out.println("\nGracias por usar nuestros menús!!!\n");
     }
 }
