b57b0d771a29b207a59a8dc53bad7c232fe05a25
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2024-03-09 11:07:22 -0500

    Se cambia el modo de suo del switch

--- Talleres/T4/src/App.java
+++ Talleres/T4/src/App.java
@@ -3,57 +3,62 @@ import GestorBD.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        String[] menu = { "Inserción de datos", "Modificación de datos", "Eliminación de datos", "Recuperación de datos" };
+
+        String[] menu = { "Insercion de datos", "Modificacion de datos", "Eliminacion de datos",
+                "Recuperacion de datos" };
         String seleccion;
         GestorMenu gestorMenu = new GestorMenu();
-        GestorBD.Tablas gestorBD = new Tablas();
-
+        Tablas gestorBD = new Tablas();// Aquí cambiamos el tipo de la instancia a Tablas
+        String tablaActual;   
         do {
             seleccion = gestorMenu.showMenu("Menú Principal", menu);
-            System.out.println(seleccion);
+            System.out.println(seleccion + "------------");
+
             switch (seleccion) {
-                case "Inserción de datos":
-                    addRegister(gestorMenu, gestorBD, "Seleccione una tabla para inserción de datos:");
+                case "Insercion de datos":
+                    tablaActual = gestorMenu.showMenu("Inserción de datos", gestorBD.getTables());
+                    String[] Campos = gestorBD.getAttributes(tablaActual);
+                    String[] nuevoRegistro = new String[Campos.length];
+                    int i = 0;
+                    for (String Campo : Campos) {
+                        System.out.println("Que desea agregar en el campo: ");
+                        nuevoRegistro[i] = "prueba";
+                        i++;
+                    }
+                    gestorBD.addRegister(tablaActual, nuevoRegistro);
+                    System.out.println("Ya se guardo");
                     break;
-                case "Modificación de datos":
-                    replaceRegister(gestorMenu, gestorBD, "Seleccione una tabla para modificación de datos:");
+                case "Recuperacion de datos":
+                    tablaActual = gestorMenu.showMenu("Recuperación de datos", gestorBD.getTables());
+                    for (String[] string : gestorBD.getRegisters(tablaActual)) {
+                        for (String string2 : string) {
+                            System.out.print(string2+"\t");
+                        }
+                        System.out.print("\n");
+                    }
                     break;
-                case "Eliminación de datos":
-                    deleteRegister(gestorMenu, gestorBD, "Seleccione una tabla para eliminación de datos:");
-                    break;
-                case "Recuperación de datos":
-                    getRegisters(gestorMenu, gestorBD, "Seleccione una tabla para recuperación de datos:");
-                    break;
-                case "Q":
-                    System.out.println("Saliendo...");
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
-                    break;
+
+                    /*
+                     * gestorBD.addRegister(gestorMenu, gestorBD);
+                     * break;
+                     * case "Modificación de datos":
+                     * gestorBD.replaceRegister(gestorMenu, gestorBD);
+                     * break;
+                     * case "Eliminación de datos":
+                     * gestorBD.deleteRegister(gestorMenu, gestorBD);
+                     * break;
+                     * case "Recuperación de datos":
+                     * gestorBD.getRegisters(gestorMenu, gestorBD);
+                     * break;
+                     * case "Q":
+                     * System.out.println("Saliendo...");
+                     * break;
+                     * default:
+                     * System.out.println("Opción no válida.");
+                     * break;/*
+                     */
             }
         } while (!seleccion.equals("Q"));
     }
 
-    private static void getRegisters(GestorMenu gestorMenu, Tablas gestorBD, String string) {
-        
-        throw new UnsupportedOperationException("Unimplemented method 'getRegisters'");
-    }
-
-    private static void deleteRegister(GestorMenu gestorMenu, Tablas gestorBD, String string) {
-    
-        throw new UnsupportedOperationException("Unimplemented method 'deleteRegister'");
-    }
-
-    private static void replaceRegister(GestorMenu gestorMenu, Tablas gestorBD, String string) {
-    
-        throw new UnsupportedOperationException("Unimplemented method 'replaceRegister'");
-    }
-
-    private static void addRegister(GestorMenu gestorMenu, Tablas gestorBD, String string) {
-    
-        throw new UnsupportedOperationException("Unimplemented method 'addRegister'");
-    }   
-        
 }
-
