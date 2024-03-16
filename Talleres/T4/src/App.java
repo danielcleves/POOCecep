c120f9b8e5280b92a5cd26f9fe9a6d7d63067db8
danielcleves <daniel.cleves.rueda@gmail.com>
2024-03-16 13:26:03 -0500

    Commit de emergencia para presentar en clase Revisar cambios

--- Talleres/T4/src/App.java
+++ Talleres/T4/src/App.java
@@ -9,7 +9,7 @@ public class App {
         String seleccion;
         GestorMenu gestorMenu = new GestorMenu();
         Tablas gestorBD = new Tablas();// Aquí cambiamos el tipo de la instancia a Tablas
-        String tablaActual;   
+        String tablaActual;
         do {
             seleccion = gestorMenu.showMenu("Menú Principal", menu);
             System.out.println(seleccion + "------------");
@@ -21,43 +21,48 @@ public class App {
                     String[] nuevoRegistro = new String[campos.length];
                     int i = 0;
                     for (String campo : campos) {
-                        nuevoRegistro[i] = gestorMenu.recibirPregunta("Que desea agregar en el campo "+campo+": " );
-                        
+                        nuevoRegistro[i] = gestorMenu.recibirPregunta("Que desea agregar en el campo " + campo + ": ");
+
                         i++;
                     }
                     gestorBD.addRegister(tablaActual, nuevoRegistro);
                     System.out.println("Ya se guardo");
                     break;
+                case "Modificacion de datos":
+                    break;
+                case "Eliminacion de datos":
+                    tablaActual = gestorMenu.showMenu("Eliminación de datos", gestorBD.getTables());
+                    gestorMenu.showTable(tablaActual, gestorBD.getAttributes(tablaActual),
+                            gestorBD.getRegisters(tablaActual));
+                    int indexRegistro = Integer.parseInt(
+                            gestorMenu.recibirPregunta("En que posición está el registro que deseas guardar?"));
+                    gestorBD.deleteRegister(tablaActual, nuevoRegistro);
+                    break;
                 case "Recuperacion de datos":
                     tablaActual = gestorMenu.showMenu("Recuperación de datos", gestorBD.getTables());
-                    for (String[] string : gestorBD.getRegisters(tablaActual)) {
-                        for (String string2 : string) {
-                            System.out.print(string2+"\t");
-                        }
-                        System.out.print("\n");
-                    }
+                    gestorMenu.showTable(tablaActual, gestorBD.getAttributes(tablaActual),
+                            gestorBD.getRegisters(tablaActual));
                     break;
-                
 
-                    /*
-                     * gestorBD.addRegister(gestorMenu, gestorBD);
-                     * break;
-                     * case "Modificación de datos":
-                     * gestorBD.replaceRegister(gestorMenu, gestorBD);
-                     * break;
-                     * case "Eliminación de datos":
-                     * gestorBD.deleteRegister(gestorMenu, gestorBD);
-                     * break;
-                     * case "Recuperación de datos":
-                     * gestorBD.getRegisters(gestorMenu, gestorBD);
-                     * break;
-                     * case "Q":
-                     * System.out.println("Saliendo...");
-                     * break;
-                     * default:
-                     * System.out.println("Opción no válida.");
-                     * break;/*
-                     */
+                /*
+                 * gestorBD.addRegister(gestorMenu, gestorBD);
+                 * break;
+                 * case "Modificación de datos":
+                 * gestorBD.replaceRegister(gestorMenu, gestorBD);
+                 * break;
+                 * case "Eliminación de datos":
+                 * gestorBD.deleteRegister(gestorMenu, gestorBD);
+                 * break;
+                 * case "Recuperación de datos":
+                 * gestorBD.getRegisters(gestorMenu, gestorBD);
+                 * break;
+                 * case "Q":
+                 * System.out.println("Saliendo...");
+                 * break;
+                 * default:
+                 * System.out.println("Opción no válida.");
+                 * break;/*
+                 */
             }
         } while (!seleccion.equals("Q"));
     }
